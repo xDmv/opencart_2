9d8d480ef9bdc17e4c0208b9928cfd8ec5e1684d
makushin dima <makushin.dima@gmail.com>
2018-02-04 16:42:08 +0200

    up

--- catalog/controller/common/header.php
+++ catalog/controller/common/header.php
@@ -112,34 +112,14 @@ class ControllerCommonHeader extends Controller {
 
 		foreach ($categories as $category) {
 			if ($category['top']) {
-				/*
-				// Level 3
-				$children_lv3_data = array();
-
-				$children_lv3 = $this->model_catalog_category->getCategories($child['category_id']);
-
-				foreach ($children_lv3 as $child_lv3) {
-					$filter_data_lv3 = array(
-						'filter_category_id'  => $child_lv3['category_id'],
-						'filter_sub_category' => true
-					);
-
-					$children_lv3_data[] = array(
-						'name'  => $child_lv3['name'] . ($this->config->get('config_product_count') ? ' (' . $this->model_catalog_product->getTotalProducts($filter_data_lv3) . ')' : ''),
-						'href'  => $this->url->link('product/category', 'path=' . $category['category_id'] . '_' . $child['category_id'] . '_' . $child_lv3['category_id'])
-					);
-				}
-*/
 				// Level 2
 				$children_data = array();
 
 				$children = $this->model_catalog_category->getCategories($category['category_id']);
 
 				foreach ($children as $child) {
-
 					// Level 3
 					$children_lv3_data = array();
-
 					$children_lv3 = $this->model_catalog_category->getCategories($child['category_id']);
 
 					foreach ($children_lv3 as $child_lv3) {
@@ -154,7 +134,6 @@ class ControllerCommonHeader extends Controller {
 						);
 					}
 					// Level 3 end
-					
 					$filter_data = array(
 						'filter_category_id'  => $child['category_id'],
 						'filter_sub_category' => true
@@ -167,8 +146,6 @@ class ControllerCommonHeader extends Controller {
 						'href'  => $this->url->link('product/category', 'path=' . $category['category_id'] . '_' . $child['category_id'])
 					);
 				}
-
-
 				// Level 1
 				$data['categories'][] = array(
 					'name'     => $category['name'],
